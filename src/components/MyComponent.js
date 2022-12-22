7881d01742b5024d0176c7c8324aa4e3c69e963e
buianhtai1205 <buianhtai2017tq@gmail.com>
2022-12-22 16:17:14 +0700

    props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,16 +2,21 @@
 // function component
 
 import React from "react";
+import DisplayInfo from "./DisplayInfo";
 import UserInfo from "./UserInfo"
 
 class MyComponent extends React.Component {
     
     // JSX
     render() {
+        const myInfo = ['a', 'ab'];
+
         return (
-        <div>
-            <UserInfo/>
-        </div>
+            <div>
+                <UserInfo/>
+                <br />
+                <DisplayInfo name={ "galaxyDEV" } age={ 20 } myInfo={ myInfo } /> 
+            </div>
         );
     }
 }
