30e03d0e7398c617d4e2b389c3031e148471b94d
buianhtai1205 <buianhtai2017tq@gmail.com>
2022-12-24 14:36:03 +0700

    life cycle react

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -4,9 +4,26 @@ import logo from "./../logo.svg";
 
 class DisplayInfo extends React.Component {
     constructor(props) {
+        console.log(">> call constructor: ", 1)
         super(props);
         this.state = {
             isShowListUSers: true,
+        }
+    }
+
+    componentDidMount = () => {
+        console.log(">> call component display mount");
+        setTimeout(() => {
+            document.title = "galaxyReact"
+        }, 3000);
+    }
+
+    componentDidUpdate = (prevProps, prevState, snapshot) => {
+        console.log(">> call component did update", this.props, prevProps);
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert("You got five users");
+            }
         }
     }
     
@@ -17,6 +34,7 @@ class DisplayInfo extends React.Component {
     }
 
     render() {
+        console.log(">> call render")
         const { listUsers } = this.props;
         return (
             <div className="display-info-container">
